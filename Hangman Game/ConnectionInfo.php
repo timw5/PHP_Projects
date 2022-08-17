deb33f893bb0b0bb6c47008b83b28cb8bf080539
Tim Williams <70343494+timw5@users.noreply.github.com>
2022-08-16 18:10:39 -0600

    Update ConnectionInfo.php

--- Hangman Game/ConnectionInfo.php
+++ Hangman Game/ConnectionInfo.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "sql102.epizy.com";
-$username = "epiz_31690265";
-$password = "mfnBMhQPilk";
-$dbname = "epiz_31690265_Hangman";
+$servername = "";
+$username = "";
+$password = "";
+$dbname = "";
 
 
 function resetSession() {
